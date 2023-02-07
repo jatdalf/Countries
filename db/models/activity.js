d1d4b3dc7f04037251e5f609cb488edfd947686c
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-02-07 19:33:26 -0300

    day one

---
+++ db/models/activity.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');  
+
+//aqui declaramos el modelo que va a manejar las actividades turisticas
+module.exports = sequelize => {
+    sequelize.define('activity', {
+      ID:{
+        type: DataTypes.INTEGER,
+        primaryKey:true,
+        allowNull:false
+      },
+      name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+      },
+      dificulty:{
+        type:DataTypes.ENUM(1, 2, 3, 4, 5),
+        allowNull:false,
+      },
+      duration:{
+        type:DataTypes.INTEGER,        
+        allowNull:false,
+      },
+      season:{
+        type: DataTypes.ENUM('Summer', 'Spring', 'Autumn', 'Winter', 'All'),
+        defaultValue:'All'
+      }
+    },{
+      timestamps:false
+    })
+  }
