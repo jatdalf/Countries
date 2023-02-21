900a53107ff20c44b4b70f88a20c93cbb8f12c75
Jorge Toso <jorgetoso@hotmail.com>
2023-02-21 18:20:41 -0300

    backend v1.3

--- api/src/db.js
+++ api/src/db.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {DB_USER, DB_PASSWORD, DB_HOST} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME} = process.env;
 
 //modelos sequelize
 // const modelCountry = require('./models/Country.js')
 // const modelActivity = require('./models/Activity.js')
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
