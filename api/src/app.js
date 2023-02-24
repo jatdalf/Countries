ca9ce9aa71d9389267c454b57305e8e425c1614f
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-02-24 08:54:27 -0300

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mainRouter = require('./routes/index.js');
+const front = "https://countries-2clwj5t8c-jatdalf.vercel.app/"
+//const front = "http://localhost:3000"
 
 require('./db.js');
 
@@ -17,7 +19,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', front); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
